814d611fa3f3cff80f0a94af4a1d891cd7b82549
Brandon Wiley <brandon@blanu.net>
2010-12-19 23:13:42 -0600

    Added doc arguement for callback

--- static/freefall.js
+++ static/freefall.js
@@ -38,6 +38,19 @@ freefall.Document=function(db, did)
   this.fullDoc=null;
 
   this.docCallback=null;
+  
+  doc=this;
+
+  this.internalDocCallback=function(data) {
+		log('doc changed '+doc.docid);
+		log(data);
+		if(doc.docCallback)
+		{
+			doc.docCallback(doc, data);
+		}
+  }
+  
+  Web2Peer.listen(this.db.dbid+'-'+this.docid, this.internalDocCallback);
 
   this.setDocCallback=function(f)
   {
@@ -48,17 +61,31 @@ freefall.Document=function(db, did)
   {
     log('get doc');
     var url=this.base+'/db/'+this.db.dbid+'/'+this.docid;
-    $.getJSON(url, this.docCallback);
+    $.getJSON(url, this.internalDocCallback);
   }
+
+  this.setName=function(name)
+  {
+		this.docid=name;
+	}
 
   this.save=function(doc)
   {
+		log('saving...');
     log(doc);
 
     this.fullDoc=doc;
 
-    var url=this.base+"/db/"+this.db.dbid+'/'+this.docid;
-    $.post(url, JSON.stringify(doc));
+    if(this.docid===null || this.docid===undefined)
+    {
+      var url=this.base+"/db/"+this.db.dbid;
+      $.post(url, JSON.stringify(doc), this.setName);
+		}
+		else
+		{
+      var url=this.base+"/db/"+this.db.dbid+'/'+this.docid;
+      $.post(url, JSON.stringify(doc));
+		}
   }
 
   return this;
@@ -103,6 +130,45 @@ freefall.submitForm=function()
 {
 	log('submitForm');
 	log(this);
+
+  var host=$(this).attr('host');
+  var base='http://'+host;
+	var dbname=$(this).attr('db');
+	var db=freefall.Database(base, dbname);
+	var docname=$(this).attr('doc');
+	var doc=db.get(docname);
+	
+	var data={};
+	$(this).find("input").each(function(index) {
+		try
+		{
+  	  var key=$(this).attr('name');
+	    var value=$(this).val();
+	  
+  	  data[key]=value;
+		}
+		catch(e)
+		{
+			log('Exception:');
+			log(e);
+		}
+  });
+	$(this).find("textarea").each(function(index) {
+		try
+		{
+  	  var key=$(this).attr('name');
+	    var value=$(this).val();
+	  
+  	  data[key]=value;
+		}
+		catch(e)
+		{
+			log('Exception:');
+			log(e);
+		}
+  });
+	
+	doc.save(data);
 	
 	return false;
 }
