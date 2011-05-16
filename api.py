47e7400f8ac4aa93cc8c9e805e4852aa626ef1e3
Brandon Wiley <brandon@blanu.net>
2011-05-16 10:22:30 -0500

    Moved to JSON-RPC interface, STM semantics

--- api.py
+++ api.py
@@ -6,25 +6,22 @@ from generic import JsonRpcService
 from models import *
 from util import *
 
-from views import runView
+from views import *
 
 class DatabaseService(JsonRpcService):
   def json_new(self, dbid):
     logging.info('new database:' +str(dbid))
 
-    try:
-      db=newDatabase(None, dbid=dbid)
-    except Exception, e:
-      logging.error(str(e))
-    if not db:
+    if Database.all().filter("dbid =", dbid).count()!=0:
       logging.error('Database with that id already exists '+str(dbid))
       return None
-    else:
-      logging.error('db: '+str(db))
-      logging.info('dbid: '+str(db.dbid))
 
-      return db.dbid
-
+    logging.info('using '+str(dbid))
+    db=Database(dbid=dbid)
+    db.save()
+    
+    return db.dbid
+  
   def json_list(self):
     dbs=Database.all().fetch(100)
     results=[]
@@ -34,7 +31,7 @@ class DatabaseService(JsonRpcService):
     return results
 
   def json_keys(self, dbid):
-    db=Database.all().filter("dbid =", dbid).get()
+    db=lookupDb(dbid)
     if not db:
       logging.error('Database with that id does not exist '+str(dbid))
       return
@@ -47,7 +44,7 @@ class DatabaseService(JsonRpcService):
     return results
 
   def json_docs(self, dbid):
-    db=Database.all().filter("dbid =", dbid).get()
+    db=lookupDb(dbid)
     if not db:
       logging.error('Database with that id does not exist '+str(dbid))
       return None
@@ -56,7 +53,7 @@ class DatabaseService(JsonRpcService):
 
 class DocumentService(JsonRpcService):
   def json_get(self, dbid, docid):
-    db=Database.all().filter("dbid =", dbid).get()
+    db=lookupDb(dbid)    
     if not db:
       logging.error('Database with that id does not exist '+str(dbid))
       return None
@@ -81,7 +78,7 @@ class DocumentService(JsonRpcService):
       return loads(doc.state)
     
   def json_modify(self, dbid, changes):
-    db=Database.all().filter("dbid =", dbid).get()
+    db=lookupDb(dbid)
     if not db:
       logging.error('Database with that id does not exist '+str(dbid))
       return None
@@ -96,7 +93,7 @@ class DocumentService(JsonRpcService):
         f=ops[op]
         f(db, *params)
       else:
-        raise(Exception('Invalid op: '+str(op)))
+        raise(Exception('Invalid op: '+str(op)))    
 
   def addChange(self, db, docid, data):    
     if not docid:
@@ -109,8 +106,9 @@ class DocumentService(JsonRpcService):
 
     doc=Document(parent=db, docid=docid, database=db, state=dumps(data))
     doc.save()
-  
-#    runView(doc);
+
+    views=Database.all().ancestor(db).filter("depends =", db).fetch(100)
+    runViews(doc, views)
         
     return doc.docid
 
@@ -145,32 +143,11 @@ class DocumentService(JsonRpcService):
     return True
           
 class ViewService(JsonRpcService):
-  def json_get(self, dbid, viewid, key):
-    dbid=args[0]
-    viewid=args[1]
-    key=req.get('key', None) # Key should already be in JSON encoding, so we can compare directly
-    logging.info("key: "+str(key))
+  def json_new(self, dbid, viewId, viewUrl):
 
-    viewdb=Database.all().filter("dbid =", dbid).get()
-    if not viewdb:
-      logging.error('Database with that id does not exist '+str(dbid))
-      return None
+    logging.info('new view:' +str(viewId)+' of '+str(dbid))
 
-    if method=='GET':
-      results=[]
-      if(key==None):
-        views=View.all().filter("database =", viewdb).filter('viewid =', viewid).fetch(100)
-      else:
-        logging.info(str(db.GqlQuery("SELECT * FROM View WHERE database = :1 AND viewid = :2", viewdb, viewid).fetch(100)[0].viewkey))
-        logging.info(str(db.GqlQuery("SELECT * FROM View WHERE database = :1 AND viewid = :2 and viewkey = :3", viewdb, viewid, key).fetch(100)))
-        logging.info(str(key))
-        views=db.GqlQuery("SELECT * FROM View WHERE database = :1 AND viewid = :2 and viewkey = :3", viewdb, viewid, key).fetch(100)
-
-      if not views:
-        logging.error('View with that id does not exist or is empty '+str(viewid))
-        return {'viewid': viewid, 'viewkey': loads(key), 'results': results}
-      for view in views:
-        if view.value!=None:
-          value=loads(view.value)
-          results.append(value)
-      return {'viewid': viewid, 'viewkey': loads(key), 'results': results}
+    db=Database(parent=db, dbid=viewId, depends=db, viewUrl=viewUrl)
+    db.save()
+    
+    return db.dbid    
